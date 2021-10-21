6b20ede74cba13f53c49a9081b5cd7b0d89fe9f2
Ndumiso Mbete <nmbete@mhg.co.za>
2021-10-21 07:48:59 +0200

    This is the version that works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { FormCaptureComponent } from './components/form-capture/form-capture.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormCaptureComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
